37903991d43268066aecb95de11679a8e8741f37
codyjs <codyjsimms@gmail.com>
2021-11-30 20:23:41 -0600

    initial commit

---
+++ src/main/java/com/codyjs/awsspringboottest/AwsspringboottestApplication.java
@@ -0,0 +1,13 @@
+package com.codyjs.awsspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsspringboottestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsspringboottestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codyjs/awsspringboottest/AwsspringboottestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codyjs.awsspringboottest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsspringboottestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
